b429a2e245d2dd78e01679662d612ce130176108
lieven.tronckoe <lieven.tronckoe@PC-TROLI2.campus.romerocollege.be>
2021-04-26 14:49:24 +0200

    Terug query voorzien

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -56,7 +56,85 @@ class MainController extends AbstractController
         return $response;
     }
     
-   public function query(Request $request)
+    public function register(LoggerInterface $logger)
+    {
+        $request = Request::createFromGlobals();
+        $hostname = $request->request->get('hostName');
+        $macAddress = $request->request->get('macAddress');
+        $wifiMacAddress = $request->request->get('wifiMacAddress');
+        $model = $request->request->get('model');
+        $serialNumber = $request->request->get('serialNumber');
+        $mediaType = $request->request->get('mediaType');
+        $diskSize = $request->request->get('diskSize');
+        $freeSpace = $request->request->get('freeSpace');
+        $ramSize = $request->request->get('ramSize');
+        $processor = $request->request->get('processor');
+        $installDateStr = $request->request->get('installDate');
+        $osVersion = $request->request->get('osVersion');
+        $errors = $request->request->get('errors');
+        $autoPilotHash = $request->request->get('autoPilotHash');
+        
+        $repository = $this->getDoctrine()->getRepository(Computer::class);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $computerByMacAddress = null;
+        if ($macAddress) {
+            $repository->findOneBy(['macAddress' => $macAddress]);
+        } elseif ($wifiMacAddress) {
+            $repository->findOneBy(['wifiMacAddress' => $wifiMacAddress]);
+        }
+
+        $computerByHostName = $repository->findOneBy(['hostname' => $hostname]);
+        $currentComputer = null;    
+
+        if(!($computerByMacAddress || $computerByHostName))
+        {
+            // None exist, create new computer
+            $currentComputer = new Computer();
+            $entityManager->persist($currentComputer);
+        } else {
+            // Both exist
+            if ($computerByMacAddress && $computerByHostName) {
+                // If a computer with the known MAC-address appears with a different hostname, assume it's removed
+                if ($computerByMacAddress->getId() != $computerByHostName->getId()) $entityManager->remove($computerByHostName);
+                $currentComputer = $computerByMacAddress;
+            } else {
+               // If only one of two is found, use that one
+               $currentComputer = $computerByHostName ?: $computerByMacAddress;
+            }
+        }
+        
+        $currentComputer->setHostName($hostname);
+        $currentComputer->setMacAddress($macAddress);
+        $currentComputer->setWifiMacAddress($wifiMacAddress);
+        $currentComputer->setErrors($errors);
+        $currentComputer->setSerialNumber($serialNumber);
+        $currentComputer->setRamSize((int)$ramSize);
+        $currentComputer->setMediaType($mediaType);
+        $currentComputer->setDiskSize((int)$diskSize);
+        $currentComputer->setFreeSpace((int)$freeSpace);
+        $currentComputer->setModel($model);
+        $currentComputer->setQueryDate(new DateTime('now', new DateTimeZone('Europe/Brussels')));
+        $installDate = DateTime::createFromFormat('YmdHis', $installDateStr, new DateTimeZone('Europe/Brussels'));
+        if ($installDate) {
+            $currentComputer->setInstallDate($installDate);
+        } else {
+            $logger->error($hostname . " has invalid date " . $installDateStr);
+        }
+        $currentComputer->setProcessor($processor);
+        $currentComputer->setOsVersion($osVersion);
+        $currentComputer->setAutoPilotHash($autoPilotHash);
+        
+        $entityManager->flush();        
+
+        $response = new Response();
+        $response->setContent('<html><body><h1>OK!</h1></body></html>');
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'text/html');
+        return $response->send();
+    }
+    
+    public function query(Request $request)
     {
         $hostname = $request->query->get('hostName');
         $macAddress = $request->query->get('macAddress');
